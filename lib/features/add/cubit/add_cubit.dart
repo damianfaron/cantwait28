e8495a46e302b9c902a96510160f48070bbc1f6b
Damian Faron <damian.faron@onet.pl>
2023-07-26 16:41:52 +0200

    implement ItemModel

--- lib/features/add/cubit/add_cubit.dart
+++ lib/features/add/cubit/add_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -8,7 +6,11 @@ part 'add_state.dart';
 class AddCubit extends Cubit<AddState> {
   AddCubit() : super(const AddState());
 
-  Future<void> add(String title, String imageURL, DateTime releaseDate,) async {
+  Future<void> add(
+    String title,
+    String imageURL,
+    DateTime releaseDate,
+  ) async {
     try {
       await FirebaseFirestore.instance.collection('items').add(
         {
---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  final String id;
+  final String title;
+  final String imageURL;
+  final DateTime relaseDate;
+
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.imageURL,
+    required this.relaseDate,
+  });
+}
