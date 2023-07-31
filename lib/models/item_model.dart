65a15d0f7a4cdb4e65863961c231ebde1a198648
Damian Faron <damian.faron@onet.pl>
2023-07-31 18:00:58 +0200

    imprave displaying dates in the app

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,11 +1,18 @@
+import 'package:intl/intl.dart';
+
 class ItemModel {
   final String id;
   final String title;
   final String imageURL;
   final DateTime relaseDate;
+
   //metoda
   String daysLeft() {
     return relaseDate.difference(DateTime.now()).inDays.toString();
+  }
+
+  String relaseDateFormatted() {
+    return DateFormat.MMMEd().format(relaseDate);
   }
 
   ItemModel({
